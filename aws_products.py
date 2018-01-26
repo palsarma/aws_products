2c2fa9809a1d2bd9ee4d8dce726c1e71696c62db
Sarma Palli <palsarma@amazon.com>
2018-01-26 16:54:52 -0600

    sort service names within each category

--- aws_products.py
+++ aws_products.py
@@ -6,7 +6,8 @@ import requests
 def quotes(instr):
     return '"{}"'.format(instr.strip())
 
-output = [ ','.join(["Category","AWS Service","Description","Link"]) ]
+header = [ ','.join(["Category","AWS Service","Description","Link"]) ]
+output = {}
 
 aws_url = 'https://aws.amazon.com'
 page = requests.get(aws_url + '/products')
@@ -15,10 +16,14 @@ wrappers = tree.xpath('//div[@class="aws-item-wrapper"]')
 
 for wrapper in wrappers:
   category = quotes(wrapper.find('a/span').text)
+  output[category] = {}
   for service in wrapper.findall('div/div/a'):
     service_name = quotes(service.text)
     service_link = quotes(''.join([aws_url,service.get('href')]))
     service_desc = quotes(service.find('span').text)
-    output.append(','.join([category,service_name,service_desc,service_link]))
+    output[category][service_name] = ','.join([service_desc,service_link])
 
-print("\n".join(output))
+print("\n".join(header))
+for category in output.keys():
+  for service in sorted(output[category].keys()):
+    print( ','.join([category,service, output[category][service]]))
