9a9566656df7095de4db13f08c5094fad38f7478
MaxymVlasov <m.vlasov@post.com>
2018-01-31 01:04:20 +0200

    PEP8 for readability

--- aws_products.py
+++ aws_products.py
@@ -1,29 +1,28 @@
+from requests import get
 from lxml import html
-from lxml import etree
-import requests
 
 
 def quotes(instr):
     return '"{}"'.format(instr.strip())
 
-header = [ ','.join(["Category","AWS Service","Description","Link"]) ]
-output = {}
+HEADER = [','.join(["Category", "AWS Service", "Description", "Link"])]
+OUTPUT = {}
 
-aws_url = 'https://aws.amazon.com'
-page = requests.get(aws_url + '/products')
-tree = html.fromstring(page.content)
-wrappers = tree.xpath('//div[@class="aws-item-wrapper"]')
+AWS_URL = 'https://aws.amazon.com'
+PAGE = get(AWS_URL + '/products')
+TREE = html.fromstring(PAGE.content)
+WRAPPERS = TREE.xpath('//div[@class="aws-item-wrapper"]')
 
-for wrapper in wrappers:
-  category = quotes(wrapper.find('a/span').text)
-  output[category] = {}
-  for service in wrapper.findall('div/div/a'):
-    service_name = quotes(service.text)
-    service_link = quotes(''.join([aws_url,service.get('href')]))
-    service_desc = quotes(service.find('span').text)
-    output[category][service_name] = ','.join([service_desc,service_link])
+for wrapper in WRAPPERS:
+    category = quotes(wrapper.find('a/span').text)
+    OUTPUT[category] = {}
+    for service in wrapper.findall('div/div/a'):
+        service_name = quotes(service.text)
+        service_link = quotes(''.join([AWS_URL, service.get('href')]))
+        service_desc = quotes(service.find('span').text)
+        OUTPUT[category][service_name] = ','.join([service_desc, service_link])
 
-print("\n".join(header))
-for category in output.keys():
-  for service in sorted(output[category].keys()):
-    print( ','.join([category,service, output[category][service]]))
+print("\n".join(HEADER))
+for category in OUTPUT:
+    for service in sorted(OUTPUT[category].keys()):
+        print(','.join([category, service, OUTPUT[category][service]]))
