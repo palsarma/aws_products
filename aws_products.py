97e0f246a7bc8ee316b990c909082e9d77e81e9a
Sarma Palli <palsarma@amazon.com>
2018-11-30 10:09:40 -0600

    refactored and added docs

--- aws_products.py
+++ aws_products.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python3
+"""
+    Prints csv of AWS Products by screen-scraping AWS products page
+"""
 
 from requests import get
 from lxml import html
 
 
 class ProductsPage:
+    """
+        AWS Products Page
+    """
     def __init__(self, aws_url, products_page):
         self.aws_url = aws_url
         self.products_url = self.aws_url + products_page
 
     def products_page_content(self):
+        """ http call to products url and return contents"""
         return get(self.products_url).content
 
     def parse_products_page(self):
+        """ parse products page
+            return list of products
+            each item is a dict """
         html_obj = html.fromstring(self.products_page_content())
-        output = {}
+        output = []
         # Parse the sections for each category
         sections = html_obj.xpath('//*[contains(@class, "lb-item-wrapper")]')
         for section in sections:
-            category = section.find('a/span').text
-            output[category] = {}
+            category = section.find('a/span').text.strip()
+            services = []
             # get details for each service in this category
             for svc in section.findall('div/div/a'):
-                service = svc.text
-                output[category][service] = {
-                    'Category': category,
-                    'Service': service,
-                    'Description': svc.find('span').text,
-                    'Link': self.aws_url + svc.get('href')
-                }
+                services.append({
+                    'Category':     category,
+                    'Service':      svc.text.strip(),
+                    'Description':  svc.find('span').text.strip(),
+                    'Link':         self.aws_url + svc.get('href').strip()
+                })
+            output += sorted(services, key=lambda x: x['Service'])
         return output
 
-
-def join_quoted_values(headings, dict_item):
-    values = []
-    for heading in headings:
-        # remove blanks before and after, enclose in quotes
-        values.append('"' + dict_item[heading].strip() + '"')
-    return ','.join(values)
-
-
 def main():
-    headings = ["Category", "Service", "Description", "Link"]
+    """ main method """
     products_page = ProductsPage(
         aws_url='https://aws.amazon.com',
         products_page='/products'
     )
-    output_dict = products_page.parse_products_page()
+    products_list = products_page.parse_products_page()
 
     # Print output
-    print(",".join(list(map(lambda x: '"' + x + '"', headings))))
-    for category in output_dict:
-        for svc in sorted(output_dict[category].keys()):
-            print(join_quoted_values(headings, output_dict[category][svc]))
-
+    quotify = lambda x: '"' + x + '"'
+    print(','.join(map(quotify, products_list[0].keys())))
+    for product in products_list:
+        print(','.join(map(quotify, product.values())))
 
 if __name__ == '__main__':
     main()
